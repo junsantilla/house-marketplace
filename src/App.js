d2d18003f8840247ae091c403082aaa98777f5ac
junsantilla <junsantilla@live.com>
2022-11-13 17:31:46 +0800

    Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import Offers from "./pages/Offers.jsx";
+import Profile from "./pages/Profile.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+
 function App() {
 	return (
-		<div>
-			<h1>Home Marketplace</h1>
-		</div>
+		<>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Explore />}></Route>
+					<Route path="/offers" element={<Offers />}></Route>
+					<Route path="/profile" element={<SignIn />}></Route>
+					<Route path="/sign-in" element={<SignIn />}></Route>
+					<Route path="/sign-up" element={<SignUp />}></Route>
+					<Route
+						path="/forgot-password"
+						element={<ForgotPassword />}
+					></Route>
+				</Routes>
+			</Router>
+		</>
 	);
 }
 
