935ae01e27e8675e71f1643ee567e4ca50281ecd
junsantilla <junsantilla@live.com>
2022-11-17 16:24:25 +0800

    Sign up functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,6 +6,7 @@ import {
 	createUserWithEmailAndPassword,
 	updateProfile,
 } from "firebase/auth";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config.js";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
@@ -43,6 +44,13 @@ function SignUp() {
 			updateProfile(auth.currentUser, {
 				displayName: name,
 			});
+
+			const formDataCopy = { ...formData };
+			delete formDataCopy.password;
+			formDataCopy.timestamp = serverTimestamp();
+
+			await setDoc(doc(db, "users", user.uid), formDataCopy);
+
 			navigate("/");
 		} catch (error) {
 			console.log(error);
