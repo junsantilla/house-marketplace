24d7bc1d58bb4b0f32000e1148c2988690fc6fea
junsantilla <junsantilla@live.com>
2022-11-19 09:18:54 +0800

    Sign Out

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { getAuth } from "firebase/auth";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 function Profile() {
-	const [user, setUser] = useState([]);
 	const auth = getAuth();
 
-	useEffect(() => {
-		console.log(auth.currentUser);
-		setUser(auth.currentUser);
-	}, []);
+	const [formData, setformData] = useState({
+		name: auth.currentUser.displayName,
+		email: auth.currentUser.email,
+	});
 
-	return <>{user.displayName}</>;
+	const navigate = useNavigate();
+
+	const onLogout = () => {
+		auth.signOut();
+		navigate("/");
+	};
+
+	return (
+		<div className="profile">
+			<header className="profileHeader">
+				<p className="pageHeader">My Profile</p>
+				<button className="logOut" type="button" onClick={onLogout}>
+					Log out
+				</button>
+			</header>
+		</div>
+	);
 }
 
 export default Profile;
