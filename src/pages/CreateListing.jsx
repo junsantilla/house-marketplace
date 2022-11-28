33ca8e0359a88d71edf44ea6f614568da30f1aa5
junsantilla <junsantilla@live.com>
2022-11-28 18:01:22 +0800

    Upload images to firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -88,6 +88,7 @@ function CreateListing() {
 			return;
 		}
 
+		// Get coords
 		let geolocation = {};
 		let location;
 
@@ -115,6 +116,62 @@ function CreateListing() {
 			geolocation.lat = latitude;
 			geolocation.lng = longitude;
 		}
+
+		// Store image in firebase
+		const storeImage = async (image) => {
+			return new Promise((resolve, reject) => {
+				const storage = getStorage();
+				const fileName = `${auth.currentUser.uid}-${
+					image.name
+				}-${uuidv4()}`;
+
+				const storageRef = ref(storage, "images/" + fileName);
+
+				const uploadTask = uploadBytesResumable(storageRef, image);
+
+				uploadTask.on(
+					"state_changed",
+					(snapshot) => {
+						const progress =
+							(snapshot.bytesTransferred / snapshot.totalBytes) *
+							100;
+						console.log("Upload is " + progress + "% done");
+						switch (snapshot.state) {
+							case "paused":
+								console.log("Upload is paused");
+								break;
+							case "running":
+								console.log("Upload is running");
+								break;
+							default:
+								break;
+						}
+					},
+					(error) => {
+						reject(error);
+					},
+					() => {
+						getDownloadURL(uploadTask.snapshot.ref).then(
+							(downloadURL) => {
+								resolve(downloadURL);
+							}
+						);
+					}
+				);
+			});
+		};
+
+		const imgUrls = await Promise.all(
+			[...images].map((image) => storeImage(image))
+		).catch(() => {
+			setLoading(false);
+			toast.error("Images not uploaded");
+			return;
+		});
+
+		console.log(imgUrls);
+
+		setLoading(false);
 	};
 
 	const onMutate = (e) => {
