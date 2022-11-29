689ce624e143ce960765cb16c86f80b05c604740
junsantilla <junsantilla@live.com>
2022-11-29 13:23:38 +0800

    Add listing details to single listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -28,7 +28,51 @@ function Listing() {
 		fetchListing();
 	}, [navigate, params.listingId]);
 
-	return <div>Listing</div>;
+	if (loading) {
+		return <Spinner />;
+	}
+
+	return (
+		<main>
+			<div className="listingDetails">
+				<p className="listingName">
+					{listing.name} - $
+					{listing.offer
+						? listing.discountedPrice
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+						: listing.regularPrice
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+				</p>
+				<p className="listingLocation">{listing.location}</p>
+				<p className="listingType">
+					For {listing.type === "rent" ? "Rent" : "Sale"}
+				</p>
+				{listing.offer && (
+					<p className="discountPrice">
+						${listing.regularPrice - listing.discountedPrice}{" "}
+						discount
+					</p>
+				)}
+
+				<ul className="listingDetailsList">
+					<li>
+						{listing.bedrooms > 1
+							? `${listing.bedrooms} Bedrooms`
+							: "1 Bedroom"}
+					</li>
+					<li>
+						{listing.bathrooms > 1
+							? `${listing.bathrooms} Bathrooms`
+							: "1 Bathroom"}
+					</li>
+					<li>{listing.parking && "Parking Spot"}</li>
+					<li>{listing.furnished && "Furnished"}</li>
+				</ul>
+			</div>
+		</main>
+	);
 }
 
 export default Listing;
