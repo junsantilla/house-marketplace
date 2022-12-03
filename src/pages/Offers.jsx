aa469840825a8c7b036b70e57647b824fb336a22
junsantilla <junsantilla@live.com>
2022-12-03 22:26:37 +0800

    Load more pagination

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import {
 	collection,
 	getDocs,
@@ -8,18 +6,17 @@ import {
 	where,
 	orderBy,
 	limit,
-	StartAfter,
+	startAfter,
 } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
-import ListingItem from "../components/ListingItem";
 import Spinner from "../components/Spinner";
+import ListingItem from "../components/ListingItem";
 
 function Offers() {
 	const [listings, setListings] = useState(null);
 	const [loading, setLoading] = useState(true);
-
-	const params = useParams();
+	const [lastFetchedListing, setLastFetchedListing] = useState(null);
 
 	useEffect(() => {
 		const fetchListings = async () => {
@@ -37,6 +34,9 @@ function Offers() {
 
 				// Execute query
 				const querySnap = await getDocs(q);
+
+				const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+				setLastFetchedListing(lastVisible);
 
 				const listings = [];
 
@@ -56,6 +56,43 @@ function Offers() {
 
 		fetchListings();
 	}, []);
+
+	// Pagination / Load More
+	const onFetchMoreListings = async () => {
+		try {
+			// Get reference
+			const listingsRef = collection(db, "listings");
+
+			// Create a query
+			const q = query(
+				listingsRef,
+				where("offer", "==", true),
+				orderBy("timestamp", "desc"),
+				startAfter(lastFetchedListing),
+				limit(10)
+			);
+
+			// Execute query
+			const querySnap = await getDocs(q);
+
+			const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+			setLastFetchedListing(lastVisible);
+
+			const listings = [];
+
+			querySnap.forEach((doc) => {
+				return listings.push({
+					id: doc.id,
+					data: doc.data(),
+				});
+			});
+
+			setListings((prevState) => [...prevState, ...listings]);
+			setLoading(false);
+		} catch (error) {
+			toast.error("Could not fetch listings");
+		}
+	};
 
 	return (
 		<div className="category">
@@ -81,6 +118,11 @@ function Offers() {
 
 					<br />
 					<br />
+					{lastFetchedListing && (
+						<p className="loadMore" onClick={onFetchMoreListings}>
+							Load More
+						</p>
+					)}
 				</>
 			) : (
 				<p>There are no current offers</p>
